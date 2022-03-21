0fef8df45ea6a5f4b316febc7c875724343f6364
Imanuel Ulbricht <me@imanuel.dev>
2022-03-21 09:40:43 +0100

    Update versions

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,11 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 
 
 class LibThingyConan(ConanFile):
     # Removed a bunch of attributes that are not needed, but you might want to
     # Add them for a real project
     name = "libthingy"
-    version = "0.9"
+    version = "0.14"
     # If the source code is going to be in the same repo as the Conan recipe,
     # there is no need to define a `source` method. The source folder can be
     # defined like this
@@ -30,3 +30,34 @@ class LibThingyConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["libthingy"]
+    #
+    # def build_requirements(self):
+    #     if self._win_bash:
+    #         if not tools.get_env("CONAN_BASH_PATH"):
+    #             self.build_requires("msys2/cci.latest")
+    #
+    # @property
+    # def _win_bash(self):
+    #     return self._settings_build.os == "Windows" and \
+    #            not self._use_nmake and \
+    #            (self._is_mingw or tools.cross_building(self, skip_x64_x86=True))
+    #
+    # @property
+    # def _settings_build(self):
+    #     return getattr(self, "settings_build", self.settings)
+    #
+    # @property
+    # def _is_msvc(self):
+    #     return str(self.settings.compiler) in ["msvc", "Visual Studio"]
+    #
+    # @property
+    # def _is_clangcl(self):
+    #     return self.settings.compiler == "clang" and self.settings.os == "Windows"
+    #
+    # @property
+    # def _is_mingw(self):
+    #     return self.settings.os == "Windows" and self.settings.compiler == "gcc"
+    #
+    # @property
+    # def _use_nmake(self):
+    #     return self._is_clangcl or self._is_msvc
